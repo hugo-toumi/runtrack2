9a3b1de19344ccc0087fc9229f439f7bb9348f28
hugo-toumi <91871524+hugo-toumi@users.noreply.github.com>
2021-11-02 11:58:22 +0100

    Add files via upload

---
+++ job01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$str="LaPlateforme" ;
+$str2="Vive" ;
+$str3="!" ;
+
+echo "$str2 $str $str3" ;
+
+/***/
+
+$val = 6 ;
+echo $val ; 
+
+$val2 = $val +4 ;
+echo $val2 ;
+
+ $myBool = true ;
+ echo $myBool ;
+
+ $myBool1 = false ;
+ echo $myBool1 ;

add6e41f45fa8761769852888501dd4cd0d8a422
hugo-toumi <91871524+hugo-toumi@users.noreply.github.com>
2021-11-03 14:53:18 +0100

    Add files via upload

---
+++ jour02/job01/job03/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+for($x = 0; $x <= 100 ; $x++){
+    
+
+if($x == 42){
+    echo ' LaPlateforme_ <br>' ;
+    }
+
+ else if ($x >0 && $x <= 20){
+     echo '<i>' .$x .'</i><br/>' ;}
+
+     else if ($x >24 && $x <= 50){
+        echo '<u>' .$x .'</u><br/>' ;}
+
+     else{
+         echo $x. '<br/>';}
+
+
+
+
+
+}

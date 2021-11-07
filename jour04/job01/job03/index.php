7a186a0e08ea412c916cb4ed8e460ebd2ae20228
hugo-toumi <91871524+hugo-toumi@users.noreply.github.com>
2021-11-07 20:01:53 +0100

    Add files via upload

---
+++ jour04/job01/job03/index.php
@@ -0,0 +1,14 @@
+<?php
+
+var_dump($_POST);
+
+echo count($_POST);
+
+?>
+
+<form method="post">
+<input type="text" name="user">
+<input type="password" name="pasword">
+<input type="submit" name="submit">
+
+</form>
